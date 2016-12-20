07405341d8224d5b036a542cce61dc98f09c6e44
Jay Zhu <constantine9888@gmail.com>
2016-12-19 21:28:22 -0700

    Added clustering plot for data visualization
    code from scikit learn example
    used PCA to reduce data dimention

--- data_analysis/data_analysis.py
+++ data_analysis/data_analysis.py
@@ -431,6 +431,55 @@ def energy_optimization(X_train, y_train, X_test, y_test, Network_Condition, ite
     # print "Average NN optimization:", float(100 * iteration - avg_NN) / iteration, "%"
     # print "Average NB optimization:", float(100 * iteration - avg_NB) / iteration, "%"
 
+def k_means_clustering():
+
+    from sklearn.cluster import KMeans
+    import matplotlib.pyplot as plt
+    from sklearn.decomposition import PCA
+
+    X_train, y_train, X_test, y_test = Get_Samples(dataset="dataset_tmp.txt", Test_Size=0.00001)
+    X_train = preprocessing.scale(X_train)
+    kmeans = KMeans(n_clusters=5, random_state=0, n_jobs=-1).fit(X_train)
+    print kmeans.score(X_train)
+
+    reduced_data = PCA(n_components=2).fit_transform(X_train)
+    kmeans = KMeans(init='k-means++', n_clusters=5, n_init=10)
+    kmeans.fit(reduced_data)
+
+    # Step size of the mesh. Decrease to increase the quality of the VQ.
+    h = .02  # point in the mesh [x_min, x_max]x[y_min, y_max].
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
+    y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+    # Obtain labels for each point in mesh. Use last trained model.
+    Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure(1)
+    plt.clf()
+    plt.imshow(Z, interpolation='nearest',
+               extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+               cmap=plt.cm.Paired,
+               aspect='auto', origin='lower')
+
+    plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=3)
+    # Plot the centroids as a white X
+    centroids = kmeans.cluster_centers_
+    plt.scatter(centroids[:, 0], centroids[:, 1],
+                marker='x', s=150, linewidths=3,
+                color='w', zorder=10)
+    plt.title('K-means clustering on the digits dataset (PCA-reduced data)\n'
+              'Centroids are marked with white cross')
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+    plt.xticks(())
+    plt.yticks(())
+    plt.show()
+
 
 if __name__=="__main__":
     # Network Condition Values
@@ -458,3 +507,6 @@ if __name__=="__main__":
 
     #Prediction
     energy_optimization(X_train, y_train, X_test, y_test, Network_Condition, iteration)
+
+    # Plot Clusters
+    k_means_clustering()
